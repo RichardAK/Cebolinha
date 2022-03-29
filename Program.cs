e7250ab796f184214d31cdc7a36dfb406f2eab51
Kauã <kau.lira@yahoo.com>
2022-03-29 10:46:22 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frasecomR, frasecomL;
+
+Console.Write("Digite uma frase: ");
+frasecomR = Console.ReadLine()!;
+
+frasecomL = frasecomR 
+.Replace("r", "l")
+.Replace("R", "L");
+
+Console.WriteLine(frasecomL); 
